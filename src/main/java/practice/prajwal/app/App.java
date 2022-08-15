b0167cd57d9ba237de231b78e1280fecd030d136
prajwal-h-v <prajji14@gmail.com>
2022-08-15 14:14:16 +0530

    Initial Commit

---
+++ src/main/java/practice/prajwal/app/App.java
@@ -0,0 +1,13 @@
+package practice.prajwal.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
